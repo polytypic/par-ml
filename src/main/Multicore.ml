01cfd89a416f3b98b8e8278fb06de8621fd40479
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-01-11 11:18:32 +0200

    Increase padding to 15 words to avoid false sharing Array.length...
    ...and mutable last word.

--- src/main/Multicore.ml
+++ src/main/Multicore.ml
@@ -1,6 +1,6 @@
 let copy_as_padded (type t) (o : t) : t =
   let o = Obj.repr o in
-  let n = Obj.new_block (Obj.tag o) (Obj.size o + 14) in
+  let n = Obj.new_block (Obj.tag o) (Obj.size o + 15) in
   for i = 0 to Obj.size o - 1 do
     Obj.set_field n i (Obj.field o i)
   done;
